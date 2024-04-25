dd6000617390a0bd2a88d0e1f657ee9279aee521
Bruno Murta <bpmurta@gmail.com>
2024-04-25 19:30:42 +0100

    Delete __init__.py

--- __init__.py
+++
@@ -1,5 +0,0 @@
-import numpy as np, scipy as sp, math as mt, cmath as cmt,  random, itertools
-import qiskit, qiskit_aer as Aer
-from qiskit import *
-from qiskit.circuit.library import RYGate, RZGate
-
